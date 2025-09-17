ed84932c4a3072ed8dc1af2fbac0c69dbcbc11e5
rex69690 <tusharkoundal913@gmail.com>
2025-09-18 02:17:54 +0530

    backend ready v2

---
+++ src/main/java/com/studentManagementSystem/Student/Management/System/model/TeacherModel.java
@@ -0,0 +1,39 @@
+package com.studentManagementSystem.Student.Management.System.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "Teacher-Data")
+public class TeacherModel {
+
+    @Id
+    private String id;
+
+    @NotBlank(message = "Username cannot be empty")
+    private String username;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6 , message = "Password length must ne atleast 6")
+    private String password;
+
+    @NotBlank(message = "Subject cannot be empty")
+    private String subject;
+
+    @NotBlank(message = "Position cannot be empty")
+    private String Position;
+    private String office;
+
+    //students list assigned to teacher
+    private List<String> studentRollNumbers = new ArrayList<>();
+}
